369f528f86cc837b1f4059e3748d776b2820a26a
DrTrevich007 <26391122+christytreviranus@users.noreply.github.com>
2022-05-10 07:21:57 -0700

    Infinity scroll project files

---
+++ script.js
@@ -0,0 +1,78 @@
+//Declare Global vars
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+let photosArray = [];
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
+// Unsplash API
+const photoMax = 10;
+const apiKey = 'YOUR_API_KEY_HERE';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${photoMax}`
+
+//Check for image loaded
+function imageLoaded() {
+    imageLoaded++;
+    if (imageLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
+// Helper Function to set attributes
+function setAttribute(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+// Create elements for photos to add to DOM
+function displayPhotos() {
+    imageLoaded = 0;
+    totalImages = photosArray.length;
+    photosArray.forEach((photo) => {
+        //Create anchor
+        const item = document.createElement('a');
+        setAttribute(item, {
+            href: photo.links.html,
+            target: '_blank'
+        })
+        //Create img
+        const img = document.createElement('img');
+        setAttribute(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
+        //Event Listener for photo loading
+        img.addEventListener('load', imageLoaded);
+        //Add frame around images
+        img.classList.add('frame')
+        //Image container element
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+    } catch (error) {
+        //Catch error here
+    }
+}
+
+// Infinite scrolling 
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+})
+
+// On Load
+getPhotos();
+
